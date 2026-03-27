eaf8073dc13aa85933552952a9af773f1f9c7c5f
Dursen Tulema <dursangudeta@gmail.com>
2026-03-27 18:11:13 -0400

    Websocket

--- server/auction-service/src/main/java/com/yorku/eecs4413/auction/WebSocketConfig.java
+++ server/auction-service/src/main/java/com/yorku/eecs4413/auction/WebSocketConfig.java
@@ -1,7 +1,5 @@
 package com.yorku.eecs4413.auction;
 
-
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
@@ -12,17 +10,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        // Clients subscribe to /topic/... to receive messages
         config.enableSimpleBroker("/topic");
-        // Clients send messages to /app/...
         config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // The WebSocket handshake URL
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*")
-                .withSockJS(); // fallback for browsers without WebSocket
+                .withSockJS();
     }
 }
