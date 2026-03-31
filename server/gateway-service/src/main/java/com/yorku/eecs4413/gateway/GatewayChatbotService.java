2e95a4800e1afb054cd97f908cdfa366a78e916b
sunnyYorku <92268146+sunnyYorku@users.noreply.github.com>
2026-03-31 16:07:20 -0400

    Changed API key

--- server/gateway-service/src/main/java/com/yorku/eecs4413/gateway/GatewayChatbotService.java
+++ server/gateway-service/src/main/java/com/yorku/eecs4413/gateway/GatewayChatbotService.java
@@ -35,7 +35,7 @@ public class GatewayChatbotService {
     public void init() {
         //This apiKey is currently under a free account and has a rate limit of 35 requests/day.
         //If the chatbot stops replying, please create a new Gemini account and insert your own key.
-    	client = Client.builder().apiKey("AIzaSyBsUCshZpHornpZYT4zo2p2ontEkOBp9mQ").build();
+    	client = Client.builder().apiKey("AIzaSyBzaWnhGDcX0l4Gsa6lrIs93ZXnFeYdoK4").build();
     }
 
     // ─── Prompt ───────────────────────────────────────────────────
